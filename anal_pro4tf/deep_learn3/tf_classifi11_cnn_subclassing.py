14d558450f6c9af62f600f0defcf7a4af27f521e
lsj8367 <lsj8367@naver.com>
2020-12-09 17:34:27 +0900

    2020-12-09 final commit!

--- anal_pro4tf/deep_learn3/tf_classifi11_cnn_subclassing.py
+++ anal_pro4tf/deep_learn3/tf_classifi11_cnn_subclassing.py
@@ -16,9 +16,6 @@ print(dset)
 for a in dset:
     print(a)
 
-
-
-
 #=========================================================
 print()
 (train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
@@ -36,3 +33,48 @@ train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels)).shuf
 test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels)).batch(28)
 print(train_ds)
 print(test_ds)
+
+# SubClassing Model
+class MyModel(Model):
+    def __init__(self): # 생성자
+        super(MyModel, self).__init__()
+        self.conv1 = Conv2D(filters=32, kernel_size=[3, 3], padding = 'same', activation = 'relu')
+        self.pool1 = MaxPool2D((2, 2))
+        self.conv2 = Conv2D(filters=32, kernel_size=[3, 3], padding = 'same', activation = 'relu')
+        self.pool2 = MaxPool2D((2, 2))
+        self.flatten = Flatten(dtype = 'float32')
+        self.d1 = Dense(64, activation = 'relu')
+        self.drop1 = Dropout(rate = 0.3)
+        self.d2 = Dense(10, activation = 'softmax')
+        
+    def call(self, inputs): # init에서 선언된 layer들을 불러와 network 구성
+        net = self.conv1(inputs)
+        net = self.pool1(net)
+        net = self.conv2(net)
+        net = self.pool2(net)
+        net = self.flatten(net)
+        net = self.d1(net)
+        net = self.drop1(net)
+        net = self.d2(net)
+        return net
+        
+model = MyModel()
+temp_inputs = tf.keras.Input(shape = (28, 28, 1))
+model(temp_inputs)
+print(model.summary())
+
+# loss, optimizer 객체 선언
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
+optimizer = tf.keras.optimizers.Adam()
+
+# 일반적인 모델 학습
+model.compile(optimizer = optimizer, loss = loss_object, metrics = ['acc'])
+model.fit(train_images, train_labels, batch_size = 128, epochs = 10, verbose = 2, max_queue_size = 10, workers = 4, use_multiprocessing=True)
+# 최대 프로세스의 수 workers = 4   멀티프로세스 use_multiprocessing=True
+
+score = model.evaluate(test_images, test_labels)
+print('test loss : ', score[0])
+print('test acc : ', score[1])
+
+print('예측값 : ', np.argmax(model.predict(test_images[:2])))
+print('실제값 : ', test_labels[:2])

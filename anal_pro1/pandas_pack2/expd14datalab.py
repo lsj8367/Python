0130c0869e0623af410f1fb202efdde793c27af5
lsj8367 <lsj8367@naver.com>
2020-12-13 00:10:51 +0900

    update expd14datalab

--- anal_pro1/pandas_pack2/expd14datalab.py
+++ anal_pro1/pandas_pack2/expd14datalab.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests # urllib랑은 다르게 제약이있을수 있음
 # 보는페이지 코드와 다르다면 headers={ 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'})
+
 data = requests.get("https://datalab.naver.com/keyword/realtimeList.naver?age=20s", headers={ 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'}).text
 #print(data)
 soup = BeautifulSoup(data, 'lxml')
